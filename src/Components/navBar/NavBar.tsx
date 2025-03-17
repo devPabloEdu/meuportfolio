f39709fa081914b6d7241331abdbecf96b22f38a
Pablo <vilelapege1113@gmail.com>
2025-03-17 16:25:57 -0300

    feat : Onclick navbar

--- src/Components/navBar/NavBar.tsx
+++ src/Components/navBar/NavBar.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { IoMdCode } from "react-icons/io";
 
 
 function NavBar() {
+
+    const [activeButton, setActiveButton] = useState<string | null >(null);
+
+    function handleButtonClick(buttonName : string, e: React.MouseEvent) {
+        e.preventDefault();
+        setActiveButton(buttonName);
+    }
+
     return <div className="NavBarContainer">
         <a href="/"><IoMdCode color="#E3371E" size={30}/></a>
 
         <ul className="NavMenuContainer">
-            <a href="/">Home</a>
-            <a href="">Sobre mim</a>
-            <a href="">Tecnologias</a>
-            <a href="">Projetos</a>
-            <a href="">Experiências</a>
+            <a href="/" className={`NavButton ${activeButton === "Home" ? "active" : ""}`} onClick={(e) => handleButtonClick("Home", e)}>Home</a>
+            <a href="/sobreMim" className={`NavButton ${activeButton === "Sobre mim" ? "active" : ""}`} onClick={(e) => handleButtonClick("Sobre mim", e)}>Sobre mim</a>
+            <a href="" className="NavButton">Tecnologias</a>
+            <a href="" className="NavButton">Projetos</a>
+            <a href="" className="NavButton">Experiências</a>
         </ul>
 
         <a href="https://www.linkedin.com/in/pablo-eduardopg/" aria-label="Linkedin Pablo Eduardo" target="_blank"><FaLinkedin color="#E3371E" size={30}/></a>
